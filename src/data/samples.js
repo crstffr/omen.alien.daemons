10604b2efd92dde98bc9cd5b0976089454ee38c5
Christpher Mason <crstffr@gmail.com>
2017-12-20 01:08:14 -0600

    nedb-multi does not support autoLoad for some reason

--- src/data/samples.js
+++ src/data/samples.js
@@ -2,11 +2,12 @@ import path from 'path';
 import NeDB from 'nedb-multi';
 import settings from '../../settings';
 
-let DataStore = NeDB(settings.server.port.nedb);
+let Datastore = NeDB(settings.server.port.nedb);
 
-let samples = new DataStore({
-    filename: path.join(settings.path.user.data, 'samples.db'),
-    autoload: true
+let samples = new Datastore({
+    filename: path.join(settings.path.user.data, 'samples.db')
 });
+
+samples.loadDatabase();
 
 export default samples;
