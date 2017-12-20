77d0f3572f5b42923f6777fdd031ff493877f43b
Christpher Mason <crstffr@gmail.com>
2017-12-20 00:38:38 -0600

    Removed start/stop/restart scripts in favor of pm2

--- src/servers/waveform/index.js
+++ src/servers/waveform/index.js
@@ -1,15 +1,2 @@
-import path from 'path';
-import mkdirp from 'mkdirp';
-import Daemon from 'start-stop-daemon';
-import settings from '../../../settings';
 import Server from './server';
-
-const logPath = path.join(settings.path.user.logs, 'waveform');
-mkdirp.sync(logPath);
-
-Daemon({
-    outFile: path.join(logPath, 'out.log'),
-    errFile: path.join(logPath, 'err.log'),
-}, () => {
-    let server = new Server();
-});
+let server = new Server();
