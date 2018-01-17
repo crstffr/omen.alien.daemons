d335bad34d76125b8f9596b68de0fbe3e5693ffd
Christpher Mason <crstffr@gmail.com>
2018-01-16 20:13:03 -0600

    java + json sucks

--- src/servers/data/server.js
+++ src/servers/data/server.js
@@ -119,6 +119,7 @@ export default class WaveformServer extends SocketServer {
                     return {
                         id: sample._id,
                         name: sample.name,
+                        selected: false,
                         length: sample.files[sample.current].duration,
                         created: sample.files[sample.current].created || 0
                     }
